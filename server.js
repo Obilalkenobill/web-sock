1de0c93560d49a35f3020434e65dc04751082c4b
unknown <arbreplantebuisson@gmail.com>
2022-06-15 19:07:22 +0200

    'ok'

---
+++ server.js
@@ -0,0 +1,42 @@
+const WebSocket = require('ws');
+const http=require('http');
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+const bserver=http.createServer(app);
+const webPort = 3000;
+
+ bserver.listen(webPort, function(){
+ console.log('Web server start. http://localhost:' + webPort );
+});
+const wss=new WebSocket.Server({server:bserver});
+
+wss.on('connection',ws=>{
+ws.room=[];
+ws.send(JSON.stringify({msg:"user joined"}));
+console.log('connected');
+ws.on('message', message=>{
+    //try{
+        var messag=JSON.parse(message);
+        //}catch(e){console.log(e)}
+        console.log('messag: ',messag);
+        if(messag.join){ws.room.push(messag.join)}
+if(messag.room){console.log('room: ',messag.room);broadcast(message);}
+if(messag.msg){console.log('message: ',messag.msg)}
+})
+
+ws.on('error',e=>console.log(e))
+ws.on('close',(e)=>console.log('websocket closed'+e))
+
+})
+
+function broadcast(message){
+wss.clients.forEach(client=>{
+if(client.room.indexOf(JSON.parse(message).room)>-1){
+    console.log("LOG DU MSG :",message);
+client.send(message)
+}
+})
+}
