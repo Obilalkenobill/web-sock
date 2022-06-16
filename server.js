11701c117ad4d180e62e3deb3ce27f26c461958e
unknown <arbreplantebuisson@gmail.com>
2022-06-16 21:12:13 +0200

    'ok'

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const wss=new WebSocket.Server({server:bserver});
 wss.on('connection',ws=>{
 ws.room=[];
 ws.send(JSON.stringify({msg:"user joined"}));
-this.doTimer();
+doTimer();
 console.log('connected');
 ws.on('message', message=>{
     //try{
@@ -44,7 +44,7 @@ client.send(message)
 })
 }
 
-async function doTimer () {
+async function doTimer() {
   setInterval(() => {
     wss.clients.forEach((client) => {
       client.send(new Date().toTimeString());
